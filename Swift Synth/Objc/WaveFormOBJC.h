9d32777c71be45e92984b71894661070070cc85f
Nestor Garcia <nestorgtrabazo@gmail.com>
2020-06-16 22:51:39 +0200

    feat(waveform): add waveform objc enum.

---
+++ Swift Synth/Objc/WaveFormOBJC.h
@@ -0,0 +1,17 @@
+//
+//  WaveFormOBJC.h
+//  Swift Synth
+//
+//  Created by Nestor Garcia on 16/06/2020.
+//  Copyright © 2020 Grant Emerson. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+typedef NS_ENUM(NSInteger, WaveFormOBJC) {
+    kSine,
+    kTriangle,
+    kSawtooth,
+    kSquare,
+    kWhiteNoise
+};
